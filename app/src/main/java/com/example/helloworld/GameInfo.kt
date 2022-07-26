cb54ccd0854d50f67aa4832109e9e8d5aa053d85
TARUC <ganhx-wm19@student.taruc.edu.my>
2022-07-26 15:58:05 +0800

    week 4

---
+++ app/src/main/java/com/example/helloworld/GameInfo.kt
@@ -0,0 +1,3 @@
+package com.example.helloworld
+
+data class GameInfo(var playerName:String = "", var score: String ="")
--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,30 +1,50 @@
 package com.example.helloworld
 
+import android.content.Context
+import android.inputmethodservice.InputMethodService
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.*
+import androidx.databinding.DataBindingUtil
+import com.example.helloworld.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     lateinit var diceImg : ImageView
     lateinit var numberText : TextView
     lateinit var nameText : TextView
-    lateinit var playerrNameText : TextView
-
+    lateinit var playerNameText : TextView
+    private val playerName: GameInfo = GameInfo(playerName = "Ready Player One?")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        diceImg = findViewById(R.id.dice_image)
-        numberText = findViewById(R.id.numberText)
-        nameText = findViewById(R.id.editPlayerName)
-        playerrNameText = findViewById(R.id.playerName)
-        val rollButton: Button = findViewById(R.id.rollButton)
-        rollButton.setOnClickListener{rollDice()}
-        val subBtn: Button = findViewById(R.id.subBtn)
-        subBtn.setOnClickListener { submitName(it) }
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.gameInfo = playerName
+        //diceImg = findViewById(R.id.dice_image)
+        //numberText = findViewById(R.id.numberText)
+        //nameText = findViewById(R.id.editPlayerName)
+        //playerNameText = findViewById(R.id.playerName)
+        //val rollButton: Button = findViewById(R.id.rollButton)
+        //rollButton.setOnClickListener{rollDice()}
+        //val subBtn: Button = findViewById(R.id.subBtn)
+        //subBtn.setOnClickListener { submitName(it) }
+
+        binding.rollButton.setOnClickListener{rollDice()}
+        binding.subBtn.setOnClickListener{submitName(it)}
+
     }
-    private fun submitName(view: View){
-        playerrNameText.text = nameText.text
+    private fun submitName(view: View) {
+        //playerNameText.text = nameText.text
+        binding.apply{
+            binding.playerName.text = binding.editPlayerName.text
+            binding.playerName.clearComposingText()
+        }
+        //binding.playerName.text = binding.editPlayerName.text
+        //binding.editPlayerName.text.clear()
+        //val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        //imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
 
@@ -32,9 +52,7 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, "Roll!",Toast.LENGTH_SHORT).show()
 
         val randomNum = (1..6).random()
-        //val numberText: TextView = findViewById(R.id.numberText)
-        numberText.text = randomNum.toString()
-        //val diceImg : ImageView = findViewById(R.id.dice_image)
+        //numberText.text = randomNum.toString()
         val imgSrc = when(randomNum){
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
@@ -43,6 +61,8 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        diceImg.setImageResource(imgSrc)
+        binding.diceImage.setImageResource(imgSrc)
+        binding.numberText.text = randomNum.toString()
+        //diceImg.setImageResource(imgSrc)
     }
 }
